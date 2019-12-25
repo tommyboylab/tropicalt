c7116526b9215331118b4af7311f39bb4d7ce7bf
Thomas Fiala <thomas.fiala@oaks-lab.com>
2019-12-25 01:04:37 +0100

    Changed Error Page
    - Changed error image, text

--- components/Other/Error/Error.tsx
+++ components/Other/Error/Error.tsx
@@ -2,6 +2,6 @@ const s = require('./Error.scss');
 export default error => (
     <div className={s.error}>
         <img src="/static/err.png" />
-        <h2>Well, I guess I F**ked Up</h2>
+        <h2>Who you gonna call?</h2>
     </div>
 );

bfcc05a1101cb02929a6120fb91cb0885c50841b
Thomas Fiala <thomasalexanderfiala@gmail.com>
2020-03-11 20:38:44 +0100

    - Fixed overflow issues since refactor

--- pages/vlog.tsx
+++ pages/vlog.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Videos from '../components/Video/List';
+import Nav from '../components/Nav/Nav';
 
 export default function VlogPage(): JSX.Element {
   return (
     <main>
+      <Nav />
       <Videos />
     </main>
   );

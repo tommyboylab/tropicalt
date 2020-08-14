62f3c5002f69e935337a51ae78da7ed8aff4487e
Thomas Fiala <thomasfiala@avcd.cz>
2020-08-14 21:33:39 +0200

    Update provider

--- pages/connect/[provider].tsx
+++ pages/connect/[provider].tsx
@@ -1,6 +1,6 @@
 import React, { useLayoutEffect } from 'react';
 import { useRouter } from 'next/router';
-import { setCookie, parseCookies } from 'nookies';
+import { setCookie } from 'nookies';
 import axios from 'redaxios';
 
 const Callback = (): JSX.Element => {
@@ -8,37 +8,26 @@ const Callback = (): JSX.Element => {
   const provider = router.query.provider;
   const accessToken = router.query.access_token;
   const redirectURL = `https://api.tropicalt.ca/auth/${provider}/callback?access_token=${accessToken}`;
-  const authToken = parseCookies();
-
-  if (authToken === null) {
-    useLayoutEffect(() => {
-      axios
-        .get(redirectURL)
-        .then((res: any) => {
-          console.log(res);
-          setCookie(undefined, 'token', res.data.jwt, {
-            maxAge: 30 * 24 * 60 * 60,
-            path: '/',
-          });
-        })
-        .catch((error: any) => {
-          console.log(error);
-        })
-        .then(() => {
-          return <h3>Done.</h3>;
+  useLayoutEffect(() => {
+    axios
+      .get(redirectURL)
+      .then((res: any) => {
+        console.log(res);
+        setCookie(undefined, 'token', res.data.jwt, {
+          maxAge: 30 * 24 * 60 * 60,
+          path: '/',
         });
-    }, []);
-    return (
-      <div>
-        <h2>Retreving your token and storing it in a cookie</h2>
-      </div>
-    );
-  } else {
-    router.push('/');
-  }
+      })
+      .catch((error: any) => {
+        console.log(error);
+      })
+      .then(() => {
+        return <h3>Done.</h3>;
+      });
+  }, []);
   return (
     <div>
-      <h2>Yo</h2>
+      <h2>Retreving your token and storing it in a cookie</h2>
     </div>
   );
 };

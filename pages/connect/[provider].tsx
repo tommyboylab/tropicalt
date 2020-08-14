dff2bf6e4a05ef4ee1aede35fd0ba38e934c4d1a
Thomas Fiala <thomasfiala@avcd.cz>
2020-08-14 21:20:01 +0200

    Completed Provider

--- pages/connect/[provider].tsx
+++ pages/connect/[provider].tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { useRouter } from 'next/router';
+import { setCookie, parseCookies } from 'nookies';
+import axios from 'redaxios';
 
 const Callback = (): JSX.Element => {
   const router = useRouter();
-
-  const authToken = router.query;
+  const provider = router.query.provider;
+  const accessToken = router.query.access_token;
+  const redirectURL = `https://api.tropicalt.ca/auth/${provider}/callback?access_token=${accessToken}`;
+  const authToken = parseCookies();
 
   if (authToken === null) {
-    console.log(authToken);
+    useLayoutEffect(() => {
+      axios
+        .get(redirectURL)
+        .then((res: any) => {
+          console.log(res);
+          setCookie(undefined, 'token', res.data.jwt, {
+            maxAge: 30 * 24 * 60 * 60,
+            path: '/',
+          });
+        })
+        .catch((error: any) => {
+          console.log(error);
+        })
+        .then(() => {
+          return <h3>Done.</h3>;
+        });
+    }, []);
     return (
       <div>
         <h2>Retreving your token and storing it in a cookie</h2>
@@ -16,7 +36,6 @@ const Callback = (): JSX.Element => {
   } else {
     router.push('/');
   }
-  console.log(authToken);
   return (
     <div>
       <h2>Yo</h2>

b276c00fc33ecc98b1b4f2522db5c671afa82013
thomasfiala <thomasfiala@avocode.com>
2020-09-12 14:23:35 +0200

    see if this works

--- components/Blog/Post/Comments/CommentList/CommentList.tsx
+++ components/Blog/Post/Comments/CommentList/CommentList.tsx
@@ -97,7 +97,7 @@ const CommentList = ({ article }: any): JSX.Element => {
   const { data, error, loading } = useQuery(getCommentList, { variables: { slug } });
   const { data: userData, error: userError, loading: userLoading } = useQuery(getUser);
   if (loading && userLoading && !data && !userData) return <Load />;
-  if (error && document !== undefined) return <>{userError ? <Modal /> : <p> Some Other Error</p>}</>;
+  if (userError && error && window.document !== undefined) return <>{userError ? <Modal /> : <></>}</>;
 
   const comments = data?.comments as CommentList[];
 

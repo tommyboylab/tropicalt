68755799996e06c938f2d403f48d3592c9eaae72
thomasfiala <thomasfiala@avocode.com>
2020-10-09 17:59:14 +0200

    fix: update header

--- components/Blog/Post/Comments/CommentHeader/CommentHeader.tsx
+++ components/Blog/Post/Comments/CommentHeader/CommentHeader.tsx
@@ -7,13 +7,15 @@ type commentHeader = {
 const CommentHeader = ({ totalComments }: commentHeader): JSX.Element => {
   return (
     <div className={s.commentHeader}>
-      {totalComments > 0 && (
+      {totalComments > 0 ? (
         <>
           <h3>Comments:</h3>
           <h3>
             {totalComments} {totalComments > 1 ? 'Comments' : 'Comment'}
           </h3>
         </>
+      ) : (
+        <h3>Leave a comment below</h3>
       )}
     </div>
   );

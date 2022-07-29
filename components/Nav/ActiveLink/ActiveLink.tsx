ec6c476980f138779ef00169817b390d4c407e12
Thomas Fiala <thomasalexanderfiala@gmail.com>
2022-07-29 10:50:43 +0200

    fix: move modal to correct position, some cleanup

--- components/Nav/ActiveLink/ActiveLink.tsx
+++ components/Nav/ActiveLink/ActiveLink.tsx
@@ -2,10 +2,6 @@ import { NextRouter, withRouter } from 'next/router';
 import React from 'react';
 import Link from 'next/link';
 
-// const getStyle = (router: NextRouter, href: string) => ({
-//   color: router.pathname === href ? 'rgb(190, 55, 250)' : 'rgba(190, 55, 250, .35)',
-// });
-
 type Active = {
   children: JSX.Element;
   router: NextRouter;
@@ -13,14 +9,6 @@ type Active = {
 };
 
 const ActiveLink = ({ children, router, href }: Active) => {
-  // const handleClick: MouseEventHandler = useCallback(
-  //   (e) => {
-  //     e.preventDefault();
-  //     void router.push(href).then(() => window.scrollTo(0, 0));
-  //   },
-  //   [href, router]
-  // );
-
   return (
     <Link className={router.pathname == href ? 'active' : ''} href={href}>
       {children}
